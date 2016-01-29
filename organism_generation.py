0c3f8738344e168a062510580937cb0175c0d0a5
Caelyn McAulay <caelyn@ceruleanrodent.com>
2016-01-28 23:53:26 -0500

    skeleton of organism generation

---
+++ organism_generation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+class OrganismGenerator:
+  def __init__(self, relationship_dict):
+    self.relationships = relationship_dict
+
+  def generate_organisms(self):
+    # Proceed down the tree, creating a dict of label to  organism object
+    return True
