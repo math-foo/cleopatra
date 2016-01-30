ffbf443b3a5f899344f3347fa5d217c3ea9d84e9
Caelyn McAulay <caelyn@ceruleanrodent.com>
2016-01-29 22:46:04 -0500

    adding some tests

--- organism_generation_test.py
+++ organism_generation_test.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python
 
-class TestOrganismGenerator(unittest.testcase):
+import unittest
+
+class TestOrganismGenerator(unittest.TestCase):
+  def test_isolated_organism(self):
+    self.assertTrue(False)
+
+  def test_one_parent_organism(self):
+    self.assertTrue(False)
+
+  def test_two_parent_organism(self):
+    self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
