0e7adde5a348db36de1ef90b593e81876988a4e4
Caelyn McAulay <caelyn@ceruleanrodent.com>
2016-01-31 13:22:07 -0500

    More tests for orgnaism generation

--- organism_generation_test.py
+++ organism_generation_test.py
@@ -3,8 +3,20 @@
 import unittest
 from organism_generation import *
 
+# given A, b -> [AB, Ab, aB, ab]
+def generate_genetic_pairs(a, b):
+   if a > b:
+     return generate_genetic_pairs(b,a)
+   values = []
+   values.append(a.upper()+b.upper())
+   values.append(a.upper()+b.lower())
+   values.append(b.upper()+a.lower())
+   values.append(a.lower()+b.lower())
+   return values
+
 class TestOrganismGenerator(unittest.TestCase):
-  def setUp(self):
+  @classmethod
+  def setUpClass(self):
     relationships = {'A' : [], 'B': [], 'C': ['A'], 'D' : ['A', 'B']}
     self.organisms = OrganismGenerator(relationships)
     self.organisms.generate_organisms()
@@ -17,10 +29,24 @@ class TestOrganismGenerator(unittest.TestCase):
     self.assertEqual([], filtered_genes)
 
   def test_one_parent_organism(self):
-    self.assertTrue(False)
+    organism = self.organisms.organism_dict['C']
+    genes = [gene_entry.gene for gene_entry in organism.genes]
+    value_a = self.organisms.organism_dict['A'].genes[0].gene[0].lower()
+    values = genes[0].lower()
+    self.assertIn(value_a, values)
+    value_b = values[1] if value_a == values[0] else values[0]
+    values = generate_genetic_pairs(value_a, value_b)
+    filtered_genes = filter(lambda x: x not in values, genes)
+    self.assertEqual([], filtered_genes)
 
   def test_two_parent_organism(self):
-    self.assertTrue(False)
+    organism = self.organisms.organism_dict['D']
+    genes = [gene_entry.gene for gene_entry in organism.genes]
+    value_a = self.organisms.organism_dict['A'].genes[0].gene[0].lower()
+    value_b = self.organisms.organism_dict['B'].genes[0].gene[0].lower()
+    values = generate_genetic_pairs(value_a, value_b)
+    filtered_genes = filter(lambda x: x not in values, genes)
+    self.assertEqual([], filtered_genes)
 
 if __name__ == '__main__':
     unittest.main()
